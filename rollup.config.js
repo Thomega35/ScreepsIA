d7de64d3020b337dfe9207a40ccb0f3b6df3b259
Thomega35 <thomega35@gmail.com>
2024-10-01 23:42:00 +0200

    💚Add BUILD_PATH var in .env to specify path or else "dist"

--- rollup.config.js
+++ rollup.config.js
@@ -5,17 +5,21 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import screeps from "rollup-plugin-screeps-world";
+import { config } from "dotenv";
+
+config();
+const build_path = process.env.BUILD_PATH || "dist";
 
 export default {
   input: "src/main.ts",
   output: {
-    file: "C:/Users/thome/AppData/Local/Screeps/scripts/screeps.com/Typescript/main.js",
+    file: build_path + "/main.js",
     format: "cjs",
     sourcemap: true
   },
 
   plugins: [
-    clear({ targets: ["C:/Users/thome/AppData/Local/Screeps/scripts/screeps.com/Typescript/*"] }),
+    clear({ targets: [build_path + "/*"] }),
     resolve({ rootDir: "src" }),
     commonjs(),
     typescript({ tsconfig: "./tsconfig.json" }),
